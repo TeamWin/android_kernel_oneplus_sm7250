b5fea21b2a06ebd1550ee6710963a7344caac572
rany <ranygh@riseup.net>
2022-07-10 17:35:30 +0300

    Revert "sys_ni: Fix cond_syscall() alias for LTO"
    This reverts commit 4ab3e7a67cb3aa7c5b027841725eefcee851d5e5.

--- include/linux/linkage.h
+++ include/linux/linkage.h
@@ -23,8 +23,10 @@
 #endif
 
 #ifndef cond_syscall
-#define cond_syscall(x) \
-	long __attribute__((weak, alias("sys_ni_syscall"))) x(void);
+#define cond_syscall(x)	asm(				\
+	".weak " __stringify(x) "\n\t"			\
+	".set  " __stringify(x) ","			\
+		 __stringify(sys_ni_syscall))
 #endif
 
 #ifndef SYSCALL_ALIAS
